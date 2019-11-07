a30c15ed8d5c52e79a4b75fbaef87e7c0476f520
Abdallah <aabedraba@gmail.com>
2019-11-07 21:44:19 +0100

    Code reorg

--- index.js
+++ index.js
@@ -6,70 +6,41 @@ const options = {
         port: process.env.PORT,
     }
     // to run local node, commet webhook and uncomment polling
-    //polling: true
+    // polling: true
 };
 const url = process.env.APP_URL || '***REMOVED***';
 const bot = new TelegramBot(TOKEN, options);
 bot.setWebHook(`${url}/bot${TOKEN}`); // comment when running local node
 
-//Adding firebase support
-var firebase = require('firebase/app');
-require('firebase/database');
-
-var firebaseConfig = {
-    apiKey: "***REMOVED***",
-    authDomain: "***REMOVED***",
-    databaseURL: "***REMOVED***",
-    projectId: "***REMOVED***",
-    storageBucket: "***REMOVED***.appspot.com",
-    messagingSenderId: "***REMOVED***",
-    appId: "1:***REMOVED***:web:467d2008f618539644ffb9"
-};
-
-firebase.initializeApp(firebaseConfig);
-var database = firebase.database();
+var utils = require('./utils.js')
 
 //------
-
-//memory of last message received
-let state = {
-    lastCommand: '',
-    lastInteractedUser: 0
-}
-
-function addUser(id, name, date) {
-    database.ref('/users/' + id).set({
-        name: name,
-        date: date.getTime()
+bot.onText(/\/join/, (msg) => {
+    utils.userExists(msg.from.id).then(exists => {
+        if (exists)
+            bot.sendMessage(msg.chat.id, "You're already playing. Try listing with /list_Judaspers");
+        else {
+            const currentDate = new Date();
+            utils.addUser(msg.from.id, msg.from.first_name, currentDate);
+            bot.sendMessage(msg.chat.id, "Welcome to the game " + msg.from.first_name + "!");
+        }
     });
-}
-
-async function userExists(id) {
-    return await database.ref('/users/' + id).once('value').then(snapshot => {
-        if (snapshot.exists())
-            return true;
-        else
-            return false;
-    })
-}
-
-//No arguments handling
-bot.onText(/\/change_date_to/, (msg) => {
-    bot.sendMessage(msg.chat.id, "Enter a valid date. Example /change_date_to 2019/02/30");
 });
 
 bot.onText(/\/change_date_to (.+)/, (msg, match) => {
     let dateText = match[1];
-    let newDate = new Date(dateText);
+    const dateIsValid = utils.checkValidDate(dateText);
 
-    if (dateText.length != 10 || dateText.length == 0) {
-        bot.sendMessage(msg.chat.id, "Enter a valid date. Example /change_date_to 2019/02/30");
+    if (!dateIsValid) {
+        bot.sendMessage(msg.chat.id, "Date is invalid. Correct format: yyyy/mm/dd");
         return;
     }
 
-    userExists(msg.from.id).then(exists => {
+    const newDate = new Date(dateText);
+    utils.userExists(msg.from.id).then(exists => {
         if (exists) {
-            addUser(msg.from.id, msg.from.first_name, newDate);
+            utils.setLastJudasDate(msg.from.id, newDate.getTime());
+            utils.addRestartToStats(msg.from.id, newDate.getTime());
             bot.sendMessage(msg.chat.id, "Date of " + msg.from.first_name + " has been updated.");
         } else {
             bot.sendMessage(msg.chat.id, msg.from.first_name + ", you're not playing. If you want to join, send /join.");
@@ -84,21 +55,18 @@ bot.onText(/\/list_Judaspers/, (msg) => {
         + "List of Judaspers"
         + "\n------------------------\n";
     // Fetching ordered data
-    database.ref('/users/').orderByChild('date').once('value').then(snapshot => {
-        snapshot.forEach(function (child) {
-            let userDate = child.val().date;
-            let daysSinceLastJudas = Math.floor((currentDate.getTime() - userDate) / (1000 * 3600 * 24));
-            message += (counter += 1) + " - " + child.val().name + ' ' + daysSinceLastJudas + ' days\n';
-        })
+    utils.getJudaspersList().then(result => {
+        message += result;
         bot.sendMessage(msg.chat.id, message);
-    });
+    })
 });
 
 bot.onText(/\/restart_me/, (msg) => {
-    userExists(msg.from.id).then(exists => {
+    utils.userExists(msg.from.id).then(exists => {
         if (exists) {
             const currentDate = new Date();
-            addUser(msg.from.id, msg.from.first_name, currentDate); //re-write in database
+            utils.setLastJudasDate(msg.from.id, currentDate.getTime());
+            utils.addRestartToStats(msg.from.id, currentDate.getTime());
             bot.sendMessage(msg.chat.id, "This time you can do it! Date reset.");
         } else {
             bot.sendMessage(msg.chat.id, msg.from.first_name + ", you're not playing. If you want to join, send /join.");
@@ -106,53 +74,24 @@ bot.onText(/\/restart_me/, (msg) => {
     });
 });
 
-bot.onText(/\/join/, (msg) => {
-    userExists(msg.from.id).then(exists => {
-        if (exists)
-            bot.sendMessage(msg.chat.id, "You're already playing. Try listing with /list_Judaspers");
-        else {
-            const currentDate = new Date();
-            addUser(msg.from.id, msg.from.first_name, currentDate);
-            bot.sendMessage(msg.chat.id, "Welcome to the game " + msg.from.first_name + "!");
-        }
-    });
-});
-
-bot.on('polling_error', (error) => {
-    console.log(error);  // => 'EFATAL'
-});
-
-bot.onText(/\/join/, (msg) => {
-    userExists(msg.from.id).then(exists => {
-        if (exists)
-            bot.sendMessage(msg.chat.id, "You're already playing. Try listing with /list_Judaspers");
-        else {
-            const currentDate = new Date();
-            addUser(msg.from.id, msg.from.first_name, currentDate);
-            bot.sendMessage(msg.chat.id, "Welcome to the game " + msg.from.first_name + "!");
-        }
-    });
-});
-
-bot.onText(/\/set_birthdate/, (msg) => {
-    userExists(msg.from.id).then(exists => {
-        if (exists) {
-            bot.sendMessage(msg.chat.id, "Please *directly reply to this message' with your birthdate (yyyy/mm/dd)").then(response => {
-                bot.onReplyToMessage(response.chat.id, response.message_id, msg => {
-                    if ( msg.text.length == 10 ){
-                        const newBirthDate = new Date(msg.text);
-                        database.ref('/users/' + msg.from.id).update({birthdate: newBirthDate.getTime()})
-                        bot.sendMessage(msg.chat.id, "Date of " + msg.from.first_name + " updated. Check how many days you've lived with /list_days_lived");
-                    }
-                    else{
-                        bot.sendMessage(msg.chat.id, "Format is incorrect. Please start again /set_birthdate")
-                    }
-                })
-            })
-        }
-        else {
+bot.onText(/\/set_birthdate (.+)/, (msg, match) => {
+    let dateText = match[1];
+    let dateIsValid = utils.checkValidDate(dateText)
+    if (!dateIsValid) {
+        bot.sendMessage(msg.chat.id, "Date is invalid. Correct format is yyyy/mm/dd");
+        return;
+    }
+    const birthDate = new Date(dateText);
+    utils.setBirthDate(msg.from.id, birthDate.getTime()).then(dateIsSet => {
+        if (!dateIsSet) {
             bot.sendMessage(msg.chat.id, "You're not playing. If you'd like to start, please /join");
+            return;
         }
+        const daysToBirthday = utils.daysToBirthday(birthDate);
+        let message = "Date of " + msg.from.first_name + " updated\n"
+                    + "Days to your birthday " + daysToBirthday + " days\n"
+                    + "Check how many days you've lived with /list_days_lived";
+        bot.sendMessage(msg.chat.id, message);
     })
 });
 
@@ -160,27 +99,42 @@ bot.onText(/\/list_days_lived/, (msg) => {
     let message = "------------------------\n"
         + "Days lived"
         + "\n------------------------\n";
-    const currentDate = new Date();
-    database.ref('/users/').once('value').then(snapshot => {
-        snapshot.forEach(user => {
-            const userBirthDate = new Date(user.val().birthdate);
-            if ( !isNaN(userBirthDate.getTime()) )
-                message += user.val().name + " " + Math.floor((currentDate.getTime() - userBirthDate) / (1000 * 3600 * 24)) + " days\n";
-        })
+    utils.getDaysLived().then(result => {
+        message += result;
         bot.sendMessage(msg.chat.id, message);
     })
 })
 
 bot.onText(/\/list_birthdates/, (msg) => {
     let message = "------------------------\n"
-    + "Birthdays"
-    + "\n------------------------\n";
-    database.ref('/users/').once('value').then(snapshot => {
-        snapshot.forEach(user => {
-            const userBirthDate = new Date(user.val().birthdate);
-            if ( !isNaN(userBirthDate) )
-                message += user.val().name + " " + userBirthDate.toLocaleDateString("es-ES") + "\n"; 
-        })
-        bot.sendMessage(msg.chat.id, message );
+        + "Birthdays"
+        + "\n------------------------\n";
+    utils.getBirthDates().then(result => {
+        message += result;
+        bot.sendMessage(msg.chat.id, message);
     })
 })
+
+bot.onText(/\/my_stats/, (msg) => {
+    let message = "------------------------\n"
+        + "Stats of " + msg.from.first_name
+        + "\n------------------------\n";
+    utils.getPersonalStats(msg.from.id).then(result => {
+        message += result;
+        bot.sendMessage(msg.chat.id, message);
+    });
+})
+
+bot.onText(/\/global_stats/, (msg) => {
+    let message = "------------------------\n"
+        + "Global stats"
+        + "\n------------------------\n";
+    utils.getGlobalStats().then(result => {
+        message += result;
+        bot.sendMessage(msg.chat.id, message);
+    })
+})
+
+bot.on('polling_error', (error) => {
+    console.log(error);  // => 'EFATAL'
+});
---
+++ utils.js
@@ -0,0 +1,171 @@
+//Adding firebase support
+var firebase = require('firebase/app');
+require('firebase/database');
+
+var firebaseConfig = {
+    apiKey: "***REMOVED***",
+    authDomain: "***REMOVED***",
+    databaseURL: "***REMOVED***",
+    projectId: "***REMOVED***",
+    storageBucket: "***REMOVED***.appspot.com",
+    messagingSenderId: "***REMOVED***",
+    appId: "1:***REMOVED***:web:467d2008f618539644ffb9"
+};
+
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
+
+module.exports = {
+
+    getJudaspersList: async function () {
+        return await database.ref('/users/').orderByChild('date').once('value').then(snapshot => {
+            let message = "";
+            const currentDate = new Date();
+            let counter = 0;
+            snapshot.forEach(function (user) {
+                let userDate = user.val().date;
+                let daysSinceLastJudas = Math.floor((currentDate.getTime() - userDate) / (1000 * 3600 * 24));
+                message += (counter += 1) + " - " + user.val().name + ' ' + daysSinceLastJudas + ' days\n';
+            })
+            return message;
+        });
+    },
+
+    getUsers: async function () {
+        return await database.ref('/users/').once('value').then(snapshot => {
+            return snapshot.val();
+        })
+    },
+
+    addUser: function (id, name, date) {
+        database.ref('/users/' + id).set({
+            name: name,
+            date: date.getTime()
+        });
+    },
+
+    calculateLongestStreak: function (datesArray) {
+        let longestStreak = 0;
+        const currentDate = new Date();
+        for (let i = 1; i < datesArray.length; i++) {
+            const difference = Math.floor((datesArray[i] - datesArray[i - 1]) / (1000 * 3600 * 24));
+            if (difference > longestStreak)
+                longestStreak = difference;
+        }
+        return longestStreak;
+    },
+
+    addRestartToStats: function (id, newDate) {
+        database.ref('/stats/' + id).once('value').then(snapshot => {
+            if (snapshot.exists()) {
+                let dates = snapshot.val().date;
+                dates.push(newDate);
+                let newLongestStreak = module.exports.calculateLongestStreak(dates);
+                database.ref('/stats/' + id).set({
+                    longestStreak: newLongestStreak,
+                    date: dates
+                })
+            }
+            else {
+                database.ref('/stats/' + id).set({
+                    longestStreak: 0,
+                    date: [newDate]
+                });
+            }
+        })
+    },
+
+    userExists: async function (id) {
+        return await database.ref('/users/' + id).once('value').then(snapshot => {
+            if (snapshot.exists())
+                return true;
+            else
+                return false;
+        })
+    },
+
+    getPersonalStats: async function (id) {
+        return await database.ref('/stats/' + id).once('value').then(snapshot => {
+            let message = "Longest no-Judas streak: " + snapshot.val().longestStreak + "\n";
+            message += "Number of restarts: " + snapshot.val().date.length;
+            return message;
+        })
+    },
+
+    getBirthDates: async function () {
+        return await database.ref('/users/').once('value').then(snapshot => {
+            let message = "";
+            snapshot.forEach(user => {
+                const userBirthDate = new Date(user.val().birthdate);
+                if (!isNaN(userBirthDate))
+                    message += user.val().name
+                        + " "
+                        + userBirthDate.getDate() + "/"
+                        + (userBirthDate.getMonth() + 1) + "/"
+                        + userBirthDate.getFullYear() + ", "
+                        + module.exports.daysToBirthday(userBirthDate) + " days to birthday\n"
+            })
+            return message;
+        })
+    },
+
+    getDaysLived: async function () {
+        const currentDate = new Date();
+        return await database.ref('/users/').once('value').then(snapshot => {
+            let message = "";
+            snapshot.forEach(user => {
+                const userBirthDate = new Date(user.val().birthdate);
+                if (!isNaN(userBirthDate.getTime()))
+                    message += user.val().name + " " + Math.floor((currentDate.getTime() - userBirthDate) / (1000 * 3600 * 24)) + " days\n";
+            })
+            return message;
+        })
+    },
+
+    setBirthDate: async function (id, birthDate) {
+        return await module.exports.userExists(id).then(exists => {
+            if (exists) {
+                database.ref('/users/' + id).update({ birthdate: birthDate})
+                return true;
+            }
+            else return false;
+        })
+    },
+
+    checkValidDate: function (date) {
+        if (date.length == 10) {
+            newDate = new Date(date);
+            if (isNaN(newDate.getTime()))
+                return false;
+            else return true;
+        }
+        return true;
+    },
+
+    setLastJudasDate: function (id, newDate) {
+        database.ref('/users/' + id).update({ date: newDate });
+    },
+
+    getGlobalStats: async function () {
+        let message = "";
+        let users = await module.exports.getUsers();
+        return await database.ref('/stats/').once('value').then(snapshot => {
+            snapshot.forEach(stats => {
+                message += users[stats.key].name + "\n"
+                    + "  - Last Judas: " + "2" + " days ago" + "\n" //TODOOOOOOO
+                    + "  - Number of restarts: " + stats.val().date.length + "\n"
+                    + "  - Longest no-Judas streak: " + stats.val().longestStreak + "\n"
+            })
+            return message;
+        })
+    },
+
+    daysToBirthday: function(birthDate) {
+        const currentDate = new Date();
+        const day = birthDate.getDate();
+        const month = birthDate.getMonth();
+        const year = currentDate.getFullYear()+1;
+        const nextBirthday = new Date(year, month, day);
+        return Math.ceil((nextBirthday.getTime() - currentDate.getTime())/(1000 * 3600 * 24));
+    }
+}
